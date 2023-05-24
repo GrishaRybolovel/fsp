6442bdf2dda985b54a799a326daadc827ba64b43
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2023-05-25 00:53:42 +0300

    fifth commit

--- DjangoAPIFlutter/settings.py
+++ DjangoAPIFlutter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'YOUR_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.212.1', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.212.1', '127.0.0.1', 'fspmainservice.onrender.com']
 
 
 # Application definition
