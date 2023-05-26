e355486d5cd1d9a45aeca984e43e829d714a6c16
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2023-05-26 11:11:22 +0300

    Connecting Postgres

--- DjangoAPIFlutter/settings.py
+++ DjangoAPIFlutter/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'DjangoAPIFlutter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fspfarm',
+        'USER': 'fspfarm_user',
+        'PASSWORD': 'SBLoRsSl7GLuwHLYxJU57KstgpUz4Dlf',
+        'HOST': 'dpg-cho695hmbg50pioi9cqg-a',
+        'PORT': '5432'
     }
 }
 
