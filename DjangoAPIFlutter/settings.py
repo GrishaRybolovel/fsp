a4b2b5b57cff48e250f7fb1553305d5be0bcd25b
hermandyudin <dyudin.german@mail.ru>
2023-05-27 12:42:10 +0300

    Changed credentials

--- DjangoAPIFlutter/settings.py
+++ DjangoAPIFlutter/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'search',
     'rest_framework',
     'accounts',
     'rest_framework_simplejwt',
