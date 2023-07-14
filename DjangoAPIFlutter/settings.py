1d72c4c73e9e36fe934b82aee5cb132e87ec75a0
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2023-07-14 12:40:43 +0300

    Update settings.py

--- DjangoAPIFlutter/settings.py
+++ DjangoAPIFlutter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'YOUR_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.212.1', '127.0.0.1', 'fspmainservice.onrender.com']
+ALLOWED_HOSTS = ['192.168.212.1', '127.0.0.1', 'innofarm-main.onrender.com']
 
 
 # Application definition
