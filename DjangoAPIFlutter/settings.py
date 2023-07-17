a3b9c465e71a8d260d4f9deb53c8c138aa5f6926
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2023-07-17 23:10:44 +0300

    Update settings.py

--- DjangoAPIFlutter/settings.py
+++ DjangoAPIFlutter/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'DjangoAPIFlutter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'innofarm_ssu6',
-#         'USER': 'innofarm_ssu6_user',
-#         'PASSWORD': 'TJuv7EbSlG43IzdTd7YJ5NvSbmLLu9kq',
-#         'HOST': 'dpg-ciohehlph6elhbt0o8u0-a',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'innofarm_p2wh',
+        'USER': 'innofarm_p2wh_user',
+        'PASSWORD': 'cGBIwImI1Ecy3JnTvgIogHxTppmrSiBf',
+        'HOST': 'dpg-ciqpv1d9aq0dcpv9gobg-a',
+        'PORT': '5432'
+    }
+}
 
 
 
